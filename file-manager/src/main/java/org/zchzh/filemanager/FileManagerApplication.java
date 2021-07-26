1518979db12f389ba2f3dc44f0c2f78e21978089
zengchzh <825597202@qq.com>
2021-07-26 16:56:27 +0800

    feat(all):添加file-manager模块

---
+++ file-manager/src/main/java/org/zchzh/filemanager/FileManagerApplication.java
@@ -0,0 +1,17 @@
+package org.zchzh.filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zengchzh
+ * @date 2021/7/26
+ */
+
+@SpringBootApplication
+public class FileManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileManagerApplication.class, args);
+    }
+}
