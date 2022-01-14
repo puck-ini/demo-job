65e471f4e007e71baa720754b77c3133840ccba3
zengchzh <825597202@qq.com>
2022-01-14 15:56:46 +0800

    feat(short-link):添加short-link模块

---
+++ short-link/src/main/java/org/zchzh/shortlink/ShortLinkApplication.java
@@ -0,0 +1,16 @@
+package org.zchzh.shortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zengchzh
+ * @date 2022/1/14
+ */
+
+@SpringBootApplication
+public class ShortLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkApplication.class, args);
+    }
+}
